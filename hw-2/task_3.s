f7abc53a6e19eb1e5a1bc53d217921dc899816a1
Krupnikas <krupnik35@ya.ru>
2014-05-09 21:17:41 +0400

    Task 3 fixed

--- hw-2/task_3.s
+++ hw-2/task_3.s
@@ -5,27 +5,25 @@
 sum:
 	pushl	%ebp
 	movl	%esp, %ebp
-	subl	$20, %esp
-	movl	$0x00000000, %eax
-	movl	%eax, -8(%ebp)
+	subl	$16, %esp
+	fldz
+	fstpl	-16(%ebp)
 	movl	$0, -4(%ebp)
 	jmp	.L2
 .L3:
 	movl	-4(%ebp), %eax
-	sall	$2, %eax
+	sall	$3, %eax
 	addl	8(%ebp), %eax
-	flds	(%eax)
-	flds	-8(%ebp)
+	fldl	(%eax)
+	fldl	-16(%ebp)
 	faddp	%st, %st(1)
-	fstps	-8(%ebp)
+	fstpl	-16(%ebp)
 	addl	$1, -4(%ebp)
 .L2:
 	movl	-4(%ebp), %eax
 	cmpl	12(%ebp), %eax
 	jl	.L3
-	movl	-8(%ebp), %eax
-	movl	%eax, -20(%ebp)
-	flds	-20(%ebp)
+	fldl	-16(%ebp)
 	leave
 	ret
 	.size	sum, .-sum
@@ -33,9 +31,9 @@ sum:
 .LC2:
 	.string	"%d"
 .LC3:
-	.string	"%f"
+	.string	"%lf"
 .LC4:
-	.string	"%f\n"
+	.string	"%lf\n"
 	.text
 .globl main
 	.type	main, @function
@@ -43,31 +41,31 @@ main:
 	pushl	%ebp
 	movl	%esp, %ebp
 	andl	$-16, %esp
-	subl	$224, %esp
+	subl	$432, %esp
 	movl	$.LC2, %eax
-	leal	216(%esp), %edx
+	leal	424(%esp), %edx
 	movl	%edx, 4(%esp)
 	movl	%eax, (%esp)
 	call	__isoc99_scanf
-	movl	$0, 220(%esp)
+	movl	$0, 428(%esp)
 	jmp	.L6
 .L7:
-	movl	220(%esp), %eax
-	leal	0(,%eax,4), %edx
-	leal	16(%esp), %eax
+	movl	428(%esp), %eax
+	leal	0(,%eax,8), %edx
+	leal	24(%esp), %eax
 	addl	%edx, %eax
 	movl	$.LC3, %edx
 	movl	%eax, 4(%esp)
 	movl	%edx, (%esp)
 	call	__isoc99_scanf
-	addl	$1, 220(%esp)
+	addl	$1, 428(%esp)
 .L6:
-	movl	216(%esp), %eax
-	cmpl	%eax, 220(%esp)
+	movl	424(%esp), %eax
+	cmpl	%eax, 428(%esp)
 	jl	.L7
-	movl	216(%esp), %eax
+	movl	424(%esp), %eax
 	movl	%eax, 4(%esp)
-	leal	16(%esp), %eax
+	leal	24(%esp), %eax
 	movl	%eax, (%esp)
 	call	sum
 	movl	$.LC4, %eax
