9fff0d3f0facbd2e9555444bfb4d34995a8f4c11
Krupnikas <krupnik35@ya.ru>
2014-05-09 21:03:02 +0400

    Task 3 with tests

--- hw-2/task_3.s
+++ hw-2/task_3.s
@@ -1,88 +1,82 @@
 	.file	"task_3.c"
 	.text
-.globl function
-	.type	function, @function
-function:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movq	%rdi, -24(%rbp)
-	movl	%esi, -28(%rbp)
+.globl sum
+	.type	sum, @function
+sum:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$20, %esp
 	movl	$0x00000000, %eax
-	movl	%eax, -8(%rbp)
-	movl	$0, -4(%rbp)
-	movl	$0, -4(%rbp)
+	movl	%eax, -8(%ebp)
+	movl	$0, -4(%ebp)
 	jmp	.L2
 .L3:
-	movl	-4(%rbp), %eax
-	cltq
-	salq	$2, %rax
-	addq	-24(%rbp), %rax
-	movss	(%rax), %xmm0
-	movss	-8(%rbp), %xmm1
-	addss	%xmm1, %xmm0
-	movss	%xmm0, -8(%rbp)
-	addl	$1, -4(%rbp)
+	movl	-4(%ebp), %eax
+	sall	$2, %eax
+	addl	8(%ebp), %eax
+	flds	(%eax)
+	flds	-8(%ebp)
+	faddp	%st, %st(1)
+	fstps	-8(%ebp)
+	addl	$1, -4(%ebp)
 .L2:
-	movl	-4(%rbp), %eax
-	cmpl	-28(%rbp), %eax
+	movl	-4(%ebp), %eax
+	cmpl	12(%ebp), %eax
 	jl	.L3
-	movl	-8(%rbp), %eax
-	movl	%eax, -36(%rbp)
-	movss	-36(%rbp), %xmm0
+	movl	-8(%ebp), %eax
+	movl	%eax, -20(%ebp)
+	flds	-20(%ebp)
 	leave
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	function, .-function
+	.size	sum, .-sum
 	.section	.rodata
-.LC5:
+.LC2:
+	.string	"%d"
+.LC3:
 	.string	"%f"
+.LC4:
+	.string	"%f\n"
 	.text
 .globl main
 	.type	main, @function
 main:
-.LFB1:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	$0x3f800000, %eax
-	movl	%eax, -32(%rbp)
-	movl	$0x3f800000, %eax
-	movl	%eax, -28(%rbp)
-	movl	$0x40000000, %eax
-	movl	%eax, -24(%rbp)
-	movl	$0x40400000, %eax
-	movl	%eax, -20(%rbp)
-	movl	$0x40a00000, %eax
-	movl	%eax, -16(%rbp)
-	movl	$5, -4(%rbp)
-	movl	-4(%rbp), %edx
-	leaq	-32(%rbp), %rax
-	movl	%edx, %esi
-	movq	%rax, %rdi
-	call	function
-	unpcklps	%xmm0, %xmm0
-	cvtps2pd	%xmm0, %xmm0
-	movl	$.LC5, %eax
-	movq	%rax, %rdi
-	movl	$1, %eax
+	pushl	%ebp
+	movl	%esp, %ebp
+	andl	$-16, %esp
+	subl	$224, %esp
+	movl	$.LC2, %eax
+	leal	216(%esp), %edx
+	movl	%edx, 4(%esp)
+	movl	%eax, (%esp)
+	call	__isoc99_scanf
+	movl	$0, 220(%esp)
+	jmp	.L6
+.L7:
+	movl	220(%esp), %eax
+	leal	0(,%eax,4), %edx
+	leal	16(%esp), %eax
+	addl	%edx, %eax
+	movl	$.LC3, %edx
+	movl	%eax, 4(%esp)
+	movl	%edx, (%esp)
+	call	__isoc99_scanf
+	addl	$1, 220(%esp)
+.L6:
+	movl	216(%esp), %eax
+	cmpl	%eax, 220(%esp)
+	jl	.L7
+	movl	216(%esp), %eax
+	movl	%eax, 4(%esp)
+	leal	16(%esp), %eax
+	movl	%eax, (%esp)
+	call	sum
+	movl	$.LC4, %eax
+	fstpl	4(%esp)
+	movl	%eax, (%esp)
 	call	printf
 	movl	$0, %eax
 	leave
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE1:
 	.size	main, .-main
-	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-3)"
+	.ident	"GCC: (Debian 4.4.5-8) 4.4.5"
 	.section	.note.GNU-stack,"",@progbits
